578765be6c8f2529a1417f91086c44ef92f040cb
Ondrej Musil <>
2023-05-30 15:14:24 +0200

    find the index challenge

--- find-the-index-of-the-first-occurrence-in-a-string/index.ts
+++ find-the-index-of-the-first-occurrence-in-a-string/index.ts
@@ -2,15 +2,11 @@
 // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 function strStr(haystack: string, needle: string): number {
-    if (haystack.includes(needle)) {
-        return haystack.indexOf(needle);
-    } else {
-        return -1;
-    }
+    return haystack.indexOf(needle);
 }
 
 const test1 = {haystack: 'sadbutsad', needle: 'sad'};
 const test2 = {haystack: 'leetcode', needle: 'leeto'};
 const test3 = {haystack: 'dogcatmouse', needle: 'cat'};
 
-console.log(strStr(test3.haystack, test3.needle));
+console.log(strStr(test2.haystack, test2.needle));
